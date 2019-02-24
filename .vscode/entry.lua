ca3846dd180776da2cba37dc4b15e2dd9a2ca65a
chadvandy <39225561+chadvandy@users.noreply.github.com>
2019-02-23 20:31:25 -0500

    Update entry.lua

--- .vscode/entry.lua
+++ .vscode/entry.lua
@@ -1,18 +1,4 @@
-require("script/campaign/mod/!!!liche_manager")
-require("script/campaign/mod/liche_init")
+-- follow the pattern here, it reads the base folder. If it were in "script/example/mod.lua", then it'd be
+-- require("script/example/mod")
 
-require("script/campaign/main_warhammer/factions/wh2_dlc11_vmp_the_barrow_legion/wh2_dlc11_vmp_the_barrow_legion_prelude")
-require("script/campaign/main_warhammer/factions/wh2_dlc11_vmp_the_barrow_legion/wh2_dlc11_vmp_the_barrow_legion_start")
-
-require("script/lichemaster/log")
-require("script/lichemaster/ui")
-
-require("script/lichemaster/tables/startingArmy")
-require("script/lichemaster/tables/units")
-require("script/lichemaster/tables/regionNames")
-
-require("script/lichemaster/ui/ror")
-require("script/lichemaster/ui/ruins")
-require("script/lichemaster/ui/utility")
-
-require("testing2")
+require("script/example")
